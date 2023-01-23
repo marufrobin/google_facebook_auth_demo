30d96f1a20b5eacfe33f2308080783261b0ab99f
Maruf Robin <marufrobin5@gmail.com>
2023-01-23 11:49:44 +0600

    button for google and facebook login added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,8 +6,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        margin: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.lightBlue,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  padding: EdgeInsets.symmetric(vertical: 20)),
+              onPressed: () {},
+              icon: Image.asset(
+                "images/google.png",
+                scale: 30,
+              ),
+              label: Text(
+                "LogIn with Google",
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.lightBlue,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  padding: EdgeInsets.symmetric(vertical: 20)),
+              onPressed: () {},
+              icon: Image.asset(
+                "images/fb.png",
+                width: 24,
+                height: 24,
+              ),
+              label: Text(
+                "LogIn with Facebook",
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
