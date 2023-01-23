22e901e9d1e3f7f5bba3f631e96f0bef54ae7de7
Maruf Robin <marufrobin5@gmail.com>
2023-01-23 12:39:47 +0600

    homepage grid setup on-going

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,6 +7,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          color: Colors.cyan,
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                childAspectRatio: 0.8,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 20),
+            itemBuilder: (context, index) => Container(
+              color: Colors.redAccent,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
